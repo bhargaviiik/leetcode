f68bb109e2cf52f3b8f736988b0201ea08773981
Bhargavi <bhargavikulkarni1595@gmail.com>
2025-06-03 12:52:37 +0530

    Time: 0 ms (100%), Space: 40.6 MB (90.69%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,8 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n<=0)return false;
-        if(n==1)return true; //2^0=1 but 1%2 is 1..  evry power of two will become 1 at the end..
-        if(n%2!=0)return false;
-        return isPowerOfTwo(n/2);
+        return n>0 && Integer.bitCount(n)==1; //0 is not a power of 2 
+        //and Integer.bitCount(n) method returns the no. of 1's in binary representation of n 
     }
 }
