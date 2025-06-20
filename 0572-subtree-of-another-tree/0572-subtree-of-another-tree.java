4870a97cd6489ee8e59403e5e260eb458e0bf140
Bhargavi <bhargavikulkarni1595@gmail.com>
2025-06-20 14:57:27 +0530

    Time: 2 ms (97.94%), Space: 44.4 MB (53.48%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -16,19 +16,23 @@
  //Most of the tree problems are solved through recursion
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root==null ){ //on root is traversed
+        if(root==null ){
             return false;
         }
         if(isIdentical(root,subRoot)){
             return true;
         }
-        return isSubtree(root.left,subRoot)||isSubtree(root.right,subRoot) ;
+        boolean a=isSubtree(root.left,subRoot);
+        boolean b=isSubtree(root.right,subRoot);
+        return a||b;
     }
     public boolean isIdentical(TreeNode node,TreeNode subRoot){
         //its easier to check for false than true
-        if(node==null && subRoot==null)return true;   //at some point its gonna return true if it is
+        if(node==null && subRoot==null)return true;
         else if(node==null|| subRoot==null)return false;
         if(node.val!=subRoot.val)return false;
-        return isIdentical(node.left,subRoot.left) && isIdentical(node.right,subRoot.right);
+        boolean a = isIdentical(node.left,subRoot.left);
+        boolean b = isIdentical(node.right,subRoot.right);
+        return a&&b;
     }
 }
