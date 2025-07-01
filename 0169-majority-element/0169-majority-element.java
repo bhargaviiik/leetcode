4e42a8a25bc2ad0e9a2a2bfa5930e87344c8ad67
Bhargavi <bhargavikulkarni1595@gmail.com>
2025-07-01 15:47:45 +0530

    Time: 15 ms (22.62%), Space: 49.3 MB (93.13%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,8 +1,18 @@
-import java.util.Arrays;
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
-
+        HashMap<Integer,Integer> hm= new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(nums[i])){
+                hm.put(nums[i],hm.get(nums[i])+1);
+                continue;
+            }
+            hm.put(nums[i],1);
+        }
+        int n=nums.length/2;
+        Set<Integer> keySet = hm.keySet();
+        for(int key:keySet){
+            if(hm.get(key)>n) return key;
+        }
+        return -1;
     }
 }
