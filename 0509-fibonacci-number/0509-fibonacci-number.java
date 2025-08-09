70d2bf52b7476ce66ecae62336ee4986026f692e
Bhargavi <bhargavikulkarni1595@gmail.com>
2025-08-09 13:11:05 +0530

    Time: 29 ms (9.97%), Space: 44.6 MB (6.58%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,12 @@
 class Solution {
     public int fib(int n) {
-        if(n==0||n==1){
-            return n;
-        }
-        return fib(n-1)+fib(n-2);
+        int[] arr= new int[n+1];
+        return helper(n,arr);
+    }
+    int helper(int n,int[] arr){
+        if(n==0|| n==1) return n;
+        if(arr[n]!=0) return arr[n];
+        arr[n]=fib(n-1)+fib(n-2);
+        return arr[n];
     }
 }
