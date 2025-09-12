3afa095fbad76727fed2041ad838afd7a25bb44b
Bhargavi <bhargavikulkarni1595@gmail.com>
2025-09-12 10:07:01 +0530

    Time: 1 ms (77.62%), Space: 46.2 MB (27.44%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,27 +1,10 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices.length==0 || prices.length==1) return 0;
-        ArrayList<Integer> ans = new ArrayList<>();
-        for(int i=0;i<prices.length-1;i++){
-            if(prices[i+1]>prices[i]){
-                ans.add(prices[i]);
-                int end=i+1;
-                int j=i+1;
-                while(j<prices.length-1 && prices[j+1]>prices[j]){
-                    end++;
-                    j++;
-                }
-                ans.add(prices[end]);
-                i=end; //skip i to the end
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
             }
-        }
-        int maxProfit=0;
-        int i=0;
-        while(ans.size()>1 && i<ans.size()-1){
-            int a= ans.get(i);
-            int b= ans.get(i+1);
-            maxProfit+= b-a;
-            i=i+2;
         }
         return maxProfit;
     }
