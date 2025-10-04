eea83bd0345cb3db4c368a8a1ceb1d5d1be739db
Bhargavi <bhargavikulkarni1595@gmail.com>
2025-10-04 10:26:10 +0530

    Time: 0 ms (100%), Space: 44.9 MB (85.69%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,28 +1,19 @@
 class Solution {
+    void helper(int i,int j,int[][] image,boolean[][] visited, int color, int origColor,int m,int n){
+        if(i<0 || j<0 || i>=m || j>=n || visited[i][j] || image[i][j]!=origColor) return;
+        image[i][j]=color;
+        visited[i][j]=true;
+        helper(i+1,j,image,visited,color,origColor,m,n);
+        helper(i-1,j,image,visited,color,origColor,m,n);
+        helper(i,j+1,image,visited,color,origColor,m,n);
+        helper(i,j-1,image,visited,color,origColor,m,n);
+    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if (image[sr][sc] == color) return image;
-        
-        int m=image.length;
-        int n=image[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        boolean visited[][] = new boolean[m][n];
-        int orignalColor= image[sr][sc];
-        q.add(new int[]{sr,sc});
-        int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
-        while(!q.isEmpty()){
-            int[] curr=q.poll();
-            int x= curr[0];
-            int y=curr[1];
-            image[x][y]=color;
-            visited[x][y]=true;
-            for(int[] dir:directions){
-                int x2=x+ dir[0];
-                int y2=y+ dir[1];
-                if(x2>=0 && y2>=0 && x2<m && y2<n && !visited[x2][y2] && image[x2][y2]==orignalColor){
-                    q.add(new int[]{x2,y2});
-                }
-            }
-        }
+        int origColor= image[sr][sc];
+        int m= image.length;
+        int n= image[0].length;
+        boolean[][] visited = new boolean[m][n];
+        helper(sr,sc,image,visited,color,origColor,m,n);
         return image;
     }
 }
