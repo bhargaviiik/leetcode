78bcdbdf9b3890f3c0446381dea7ab8b7de4bfb0
Bhargavi <bhargavikulkarni1595@gmail.com>
2025-10-28 22:55:56 +0530

    Time: 0 ms (100%), Space: 40.7 MB (19.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,12 @@
 class Solution {
-    // this is chotu wala logic... when i know the answer for a small input, i calculate it for larger ones....
     public int climbStairs(int n) {
+        //tabulation 
         int[] dp = new int[n+1];
-        return helper(n,dp);
-    }
-    int helper(int n, int[] dp){
-        if(dp[n]!=0) return dp[n];
-        if(n==1) return 1;
-        if(n==0) return 1;
-        return dp[n]=helper(n-1,dp)+helper(n-2,dp);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
