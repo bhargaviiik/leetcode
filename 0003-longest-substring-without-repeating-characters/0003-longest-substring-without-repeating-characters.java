5c45ae4b1ec67bc3c9248955f0b6b22c9ce60555
Bhargavi <bhargavikulkarni1595@gmail.com>
2025-10-28 13:07:01 +0530

    Time: 71 ms (9.26%), Space: 45.5 MB (7.37%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,21 +1,24 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        boolean[] vis = new boolean[257];
         int max=0;
         int count=0;
         HashMap<Character, Integer> map = new HashMap<>();
         for(int i=0;i<s.length();i++){
             char curr= s.charAt(i);
-            if(!vis[curr]){
+            if(!map.containsKey(curr)){
                 count++;
-                vis[curr]=true;
                 map.put(curr,i);
             }
             else{
                 max=Math.max(max,count);
                 int idx= map.get(curr);
                 count=i-idx;  // where the repeating element lies 
-                map.put(curr,i);
+                // characters before idx have to be removed from map
+                map = new HashMap<>();
+                for(int j=idx+1;j<=i;j++){
+                    map.put(s.charAt(j),j);
+                }
+                // (map.put(curr, i))if its in the map before idx, it will get removed
             }
         }
         max= Math.max(max,count);  //max check is in repeat cond, what if it doesnt invoke
