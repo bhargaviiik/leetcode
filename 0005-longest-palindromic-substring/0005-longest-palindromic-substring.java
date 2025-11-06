d8dfd4a2d77ac099a57fa1ee80eb71fd0da2d292
Bhargavi <bhargavikulkarni1595@gmail.com>
2025-11-07 01:15:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        String ans="";
+        int max=1;
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                if(isPalindrome(s.substring(i,j+1))){
+                    if(j-i+1>max){
+                        max=j-i+1;
+                        ans= s.substring(i,j+1);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+    boolean isPalindrome(String s){
+        int i=0, j= s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            j--;
+            i++;
+        }
+        return true;
+    }
+}
