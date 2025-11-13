4041270acd28e343cb65890b358ee301e94b87ed
Bhargavi <bhargavikulkarni1595@gmail.com>
2025-11-13 19:12:06 +0530

    Time: 2 ms (98.01%), Space: 44.1 MB (7.2%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -1,18 +1,21 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
-        //insertion sort
-        for(int i=1;i<arr.length;i++){
-            int curr=arr[i];
-            int j=i-1;
-            while(j>=0 && curr<arr[j]){
-                arr[j+1]=arr[j];
-                j--;
-            }
-            arr[j+1]=curr;
+        if(arr.length==1 || arr.length==0) return true;
+        int min= Integer.MAX_VALUE, min2= Integer.MAX_VALUE;
+        HashSet<Integer> set= new HashSet<>();
+        for(int i=0;i<arr.length;i++){
+            set.add(arr[i]);
+            min=Math.min(min,arr[i]);
         }
-        int diff= arr[1]-arr[0];
-        for(int i=2;i<arr.length;i++){
-            if(arr[i]-arr[i-1]!=diff) return false;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=min) min2=Math.min(min2,arr[i]);
+        }
+        int diff=0;
+        diff= min2-min;
+        if(min2==Integer.MAX_VALUE) diff=0;
+        for(int i=1;i<arr.length;i++){
+            if(!set.contains(min+diff)) {return false; }
+            min=min+diff; 
         }
         return true;
     }
