f920f2cd2b2f3ba4ea8ba6482e41c30887f6e86e
Bhargavi <bhargavikulkarni1595@gmail.com>
2025-11-26 23:54:02 +0530

    Time: 1 ms (65.1%), Space: 47.4 MB (15.09%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,28 +1,46 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        // 1,1  
-        ArrayList<int[]> list= new ArrayList<>();
+        boolean row=false;
+        boolean column=false;
+        // Step 1: check if first row has a zero
+        for (int j = 0; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                row = true;
+                break;
+            }
+        }
+
+        // Step 2: check if first column has a zero
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                column = true;
+                break;
+            }
+        }
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j]==0){
-                    list.add(new int[]{i,j});
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-        while(!list.isEmpty()){
-            int[] curr=list.remove(0);
-            setRow(matrix,curr[0]);
-            setCol(matrix, curr[1]);
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[0].length;j++){
+                if(matrix[i][0]==0|| matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
         }
-    }
-    void setRow(int[][] matrix, int idx){
-        for(int i=0;i<matrix[0].length;i++){
-            matrix[idx][i]=0;
+        if(row){
+            for(int i=0;i<matrix[0].length;i++){
+                matrix[0][i]=0;
+            }
         }
-    }
-    void setCol(int[][] matrix, int idx){
-        for(int i=0;i<matrix.length;i++){
-            matrix[i][idx]=0;
+        if(column){ //first column
+            for(int i=0;i<matrix.length;i++){
+                matrix[i][0]=0;
+            }
         }
     }
 }
