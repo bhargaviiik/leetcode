c113ad3f24668930ecf07fbf6663c307d7d211de
Bhargavi <bhargavikulkarni1595@gmail.com>
2025-12-24 11:23:11 +0530

    Time: 1 ms (94.75%), Space: 42.7 MB (22.75%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,6 +1,10 @@
 class Solution {
     public boolean isUgly(int n) {
-        if(n==0 || n==1 || n==2 || n==3 || n==5 || n==6 || n==15 || n==10) return true;
+        if(n<=0) return false;
+        if(n<=5) return true;
+        if(n%2==0) return isUgly(n/2);
+        else if(n%3==0) return isUgly(n/3);
+        else if(n%5==0) return isUgly(n/5);
         return false;
     }
 }
