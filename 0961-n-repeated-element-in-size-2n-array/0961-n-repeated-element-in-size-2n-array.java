ccf9dac40777c66f2e318d314570e010f631c1c0
Bhargavi <bhargavikulkarni1595@gmail.com>
2026-01-02 13:31:19 +0530

    Time: 0 ms (100%), Space: 47.9 MB (6.11%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,9 +1,13 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
+        int n= nums.length;
+        for(int i=0;i<(n)/2;i++){
             if(set.contains(nums[i])) return nums[i];
             set.add(nums[i]);
+            int ele= nums[n-i-1];
+            if(set.contains(ele)) return ele;
+            set.add(ele);
         }
         return -1;
     }
