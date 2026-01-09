f1c2d9f6968b1803ec7b42a0f75a78f5f18f83e5
Bhargavi <bhargavikulkarni1595@gmail.com>
2026-01-09 11:45:49 +0530

    Time: 88 ms (92.07%), Space: 51.1 MB (75.62%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,14 +1,18 @@
 class Solution {
     public int countPrimes(int n) {
         if(n<3) return 0;
-        List<Integer> ans = new ArrayList<>();
+        if(n==3) return 1;
+        boolean[] isComp = new boolean[n];
+        int count=0;
         for(int i=2;i<n;i++){
-            boolean flag=true;
-            for(int j=0;j<ans.size();j++){
-                if(i%(ans.get(j))==0){flag=false; break;}
+            if(!isComp[i]){
+                count++;
+                if ((long) i * i >= n) continue; 
+                for(int j=i*i;j<n;j+=i){
+                    isComp[j]=true;
+                }
             }
-            if(flag) ans.add(i);
         }
-        return ans.size();
+        return count;
     }
 }
