afc276a85906c957eff14acf5992907f0e2d543e
Bhargavi <bhargavikulkarni1595@gmail.com>
2026-01-18 13:45:18 +0530

    Time: 4 ms (49.67%), Space: 52.3 MB (59.72%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,34 +1,25 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int n= grid.length;
-        int m= grid[0].length;
-        boolean[][] island= new boolean[n][m];
+        int m=grid.length;
+        int n=grid[0].length;
+        boolean visited[][] = new boolean[m][n];
         int count=0;
-        int[][] dir={{0,1},{1,0},{-1,0},{0,-1}};
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && !island[i][j]){ //new land found
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1' && !visited[i][j]){
                     count++;
-                    Queue<int[]> q = new LinkedList<>();
-                    q.add(new int[]{i,j});
-                    island[i][j]=true;
-                    while(!q.isEmpty()){  //until its empty we check for its neighbours 
-                        int[] curr=q.poll();
-                        int x= curr[0];
-                        int y= curr[1];
-                        for(int d[]:dir){
-                            int x_=x+d[0];
-                            int y_=y+d[1];
-                            if(x_>=0 && y_>=0 && x_<n && y_<m && grid[x_][y_]=='1' && !island[x_][y_]) {
-                                q.add(new int[]{x_,y_}); 
-                                island[x_][y_]=true;
-                            }
-                        }
-                        
-                    }
+                    dfs(m,n,i,j,grid,visited);
                 }
             }
         }
         return count;
+    }
+    void dfs(int m, int n, int i, int j, char[][] grid, boolean[][] visited){
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]=='0'|| visited[i][j]) return;
+        visited[i][j]=true;
+        dfs(m,n,i+1,j,grid,visited);
+        dfs(m,n,i,j+1,grid,visited);
+        dfs(m,n,i-1,j,grid,visited);
+        dfs(m,n,i,j-1,grid,visited);
     }
 }
