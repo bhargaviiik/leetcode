a65510373701e324ee33d0e9cf3aeea9e2429c88
Bhargavi <bhargavikulkarni1595@gmail.com>
2026-03-03 10:44:45 +0530

    Time: 0 ms (100%), Space: 44 MB (94.83%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,8 +1,10 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         //appraoch 2
-        int i=0;
-        while(nums[i]<nums[i+1]) i++;  //didnt include while i<n-1 cause question gurantees of atleast one peak so we'll always find it
+        int i = 0;
+        while (i < nums.length - 1 && nums[i] < nums[i + 1]) {
+            i++;
+        }
         return i;
     }
 }
