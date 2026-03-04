8d65b2f55c34ef119055d6ee757aa779a089b449
Bhargavi <bhargavikulkarni1595@gmail.com>
2026-03-04 18:59:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,17 +1,15 @@
 class Solution {
     public double myPow(double x, int n) {
-        long nn= n;
+        long nn= n;      //negative range has one extra number than positive so converting to long
         double ans=1.0;
-        if(n<0) nn*=-1;
+        if(n<0) nn*=-1;  //if negative convert to positive
         while(nn>0){
-            if(nn%2==0){
-                x=x*x;
-                nn=nn/2;
-            }
-            else{ //odd
+            if(nn%2==1){ //odd
                 ans*=x;
                 nn=nn-1;
             }
+            x=x*x;
+            nn=nn/2;
         }
         if(n<0) return 1/ans;
         return ans;
