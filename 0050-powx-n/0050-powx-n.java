2f75be5a455cf3ba7609028fcfa37a822afee66e
Bhargavi <bhargavikulkarni1595@gmail.com>
2026-03-04 18:57:43 +0530

    Time: 1 ms (9.93%), Space: 48.3 MB (21.77%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long nn= n;
+        double ans=1.0;
+        if(n<0) nn*=-1;
+        while(nn>0){
+            if(nn%2==0){
+                x=x*x;
+                nn=nn/2;
+            }
+            else{ //odd
+                ans*=x;
+                nn=nn-1;
+            }
+        }
+        if(n<0) return 1/ans;
+        return ans;
+    }
+}
