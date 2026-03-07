8439f4bda00c9c010e7964125b1f11ce0084eba1
Bhargavi <bhargavikulkarni1595@gmail.com>
2026-03-07 11:06:35 +0530

    Time: 0 ms (100%), Space: 42.6 MB (94.25%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,16 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length+2];
-        Arrays.fill(dp,-1);
-        return helper(nums,0,dp);
-    }
-    int helper(int[] nums, int i,int[] dp){
-        if(i>=nums.length){
-            return 0;
+        // recurrence relation: dp[i]=max(dp[i−1], dp[i−2]+nums[i]);
+        int prev1=0;
+        int prev2=0;
+        for(int i=0;i<nums.length;i++){
+            int curr=Math.max(prev1,prev2+nums[i]);
+            prev2=prev1;
+            prev1=curr;
         }
-        if(dp[i]!=-1) return dp[i];
-        int a= nums[i] +helper(nums,i+2,dp); //we cannot include previous taken cashes, in our ans because the order can be di
-        int b= helper(nums,i+1,dp);
-        return dp[i]= Math.max(a,b);
+        return prev1;
     }
+    //SC: O(N)
+    //dp[i-1] → skip this house
+    //dp[i-2] + nums[i] → rob this house
 }
