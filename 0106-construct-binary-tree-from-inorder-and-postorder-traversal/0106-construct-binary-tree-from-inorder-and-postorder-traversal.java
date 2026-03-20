5e815b66ae37e8863cf908c16a6649928c890ae3
Bhargavi <bhargavikulkarni1595@gmail.com>
2026-03-20 11:57:25 +0530

    Time: 1 ms (98.1%), Space: 46.6 MB (30.26%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,19 +15,18 @@
  */
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return helper(inorder, postorder, 0, inorder.length-1, 0, postorder.length-1);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<inorder.length;i++) map.put(inorder[i],i);
+        return helper(inorder, postorder, 0, inorder.length-1, 0, postorder.length-1,map);
     }
     
-    TreeNode helper(int[] inorder, int[] postorder, int i, int j, int x, int y){
+    TreeNode helper(int[] inorder, int[] postorder, int i, int j, int x, int y,HashMap<Integer,Integer> map){
         if(i>j || x>y) return null;
         TreeNode node = new TreeNode(postorder[y]);
-        int idx=0;
-        for(int a=i;a<=j;a++){
-            if(inorder[a]==postorder[y]){idx= a; break;}
-        }
+        int idx=map.get(postorder[y]);
         int leftSize=idx-i-1;
-        node.left=helper(inorder, postorder,i, idx-1, x, x+leftSize);
-        node.right= helper(inorder, postorder,idx+1, j, x+leftSize+1, y-1);
+        node.left=helper(inorder, postorder,i, idx-1, x, x+leftSize,map);
+        node.right= helper(inorder, postorder,idx+1, j, x+leftSize+1, y-1,map);
         return node;
     }
 }
