6193d08bb27e82508e5603fb7660f5b96ea445f7
Bhargavi <bhargavikulkarni1595@gmail.com>
2026-04-05 01:58:18 +0530

    Time: 1 ms (98.1%), Space: 42.5 MB (85.07%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int addDigits(int num) {
+        int ans=0;
+        while(num>9){
+            while(num!=0){
+                ans+=num%10;
+                num=num/10;
+            }
+            num=ans;
+            ans=0;
+        }
+        return num;
+    }
+}
