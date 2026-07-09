722d2c9de4385a008755add063a8cfdcde940d93
Bhargavi <bhargavikulkarni1595@gmail.com>
2026-07-09 21:49:38 +0530

    Time: 3 ms (97.03%), Space: 162.9 MB (77.23%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
@@ -1,50 +1,24 @@
 class Solution {
     public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
         boolean[] ans= new boolean[queries.length];
-        DSU dsu = new DSU(n-1);
+        int[] group = new int[n];
+        int c=0;
         for(int i=1;i<nums.length;i++){
             if(Math.abs(nums[i]-nums[i-1])<=maxDiff){
-                dsu.unionBySize(i,i-1);
+                group[i]=c;
+            }
+            else{
+                c++;
+                group[i]=c;
             }
         }
         for(int i=0;i<queries.length;i++){
-            int x= queries[i][0];
-            int y= queries[i][1];
-            int pu= dsu.findParent(x);
-            int pv= dsu.findParent(y);
-            if(pu==pv){
+            int x=queries[i][0];
+            int y=queries[i][1];
+            if(group[x]==group[y]){
                 ans[i]=true;
             }
         }
         return ans;
-    }
-    class DSU{
-        int[] parent;
-        int[] size;
-        DSU(int n){
-            parent = new int[n+1];
-            size = new int[n+1];
-            for(int i=0;i<=n;i++){
-                parent[i]=i;
-                size[i]=1;
-            }
-        }
-        int findParent(int i){
-            if(parent[i]==i) return i;
-            return parent[i]= findParent(parent[i]);
-        }
-        void unionBySize(int u, int v){
-            int pu= findParent(u);
-            int pv= findParent(v);
-            if(pu==pv) return;
-            if(size[pu]<size[pv]){
-                parent[pu]=pv;
-                size[pv]+=size[pu];
-            }
-            else{
-                parent[pv]=pu;
-                size[pu]+=size[pv];
-            }
-        }
     }
 }
